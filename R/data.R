54b985415f71c38c514405b95901ace2b929accb
paleo13 <jeffrey.hanson@uqconnect.edu.au>
2015-09-30 09:33:29 +1000

    built-in datasets

--- R/data.R
+++ R/data.R
@@ -1,1 +1,42 @@
+#' Simulated species distributions
+#' 
+#' A \code{RasterStack} of containing simulated distributions for five species. Each pixel denotes the probability of occupancy.
+#' 
+#' @docType data
+#' @keywords datasets
+#' @format \code{RasterStack}
+#' @name species
+#' @examples
+#' data(species)
+#' str(species)
+NULL
+
+#' Simulated planning units
+#' 
+#' A \code{SpatialPolygonsDataFrame} of containing simulated planning units. The columns in the data slot are 'id' (\code{integer}), 'cost' (\code{numeric}), and 'status' (\code{integer}).
+#' 
+#' @docType data
+#' @keywords datasets
+#' @format \code{SpatialPolygonsDataFrame}
+#' @name pus
+#' @examples
+#' data(pus)
+#' str(pus)
+NULL
+
+#' Simulated space data
+#' 
+#' A \code{RasterStack} of containing simulated projections for space. This space has two dimensions.
+#' 
+#' @docType data
+#' @keywords datasets
+#' @format \code{RasterStack}
+#' @name space
+#' @examples
+#' data(space)
+#' str(space)
+NULL
+
+
+
  
